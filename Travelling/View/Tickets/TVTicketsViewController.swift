bb4f97caae7f3d36093e1a8fe1fa2be478a51a67
vanluhurla <94018863+vanluhurla@users.noreply.github.com>
2024-02-20 15:51:12 +0000

    Create background view to main view controller

--- Travelling/View/Tickets/TVTicketsViewController.swift
+++ Travelling/View/Tickets/TVTicketsViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class TVTicketsViewController: UIViewController {
     
+    private var backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     let coordinator: TVTicketsCoordinator
     
     init(coordinator: TVTicketsCoordinator) {
@@ -23,6 +30,30 @@ class TVTicketsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupUI()
+    }
+}
+
+extension TVTicketsViewController {
+    func setupUI() {
+        setupViews()
+        layoutViews()
+    }
+    
+    func setupViews() {
+        view.addSubview(backgroundView)
+        
+        
+    }
+    
+    func layoutViews() {
+        NSLayoutConstraint.activate([
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.heightAnchor.constraint(equalTo: view.heightAnchor,
+                                                   multiplier: 0.3)
+        ])
     }
 }
 
